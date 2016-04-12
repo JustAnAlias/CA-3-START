f3fe700bb1cc03e4870b357b2d8f9c7db6052b68
mrulle <michael.1978@gmail.com>
2016-04-12 23:06:15 +0200

    fik den skide factory til at virke.. men beregneren er stadig ikke færdig

--- web/app/components/factories.js
+++ web/app/components/factories.js
@@ -2,50 +2,24 @@
 
 /* Place your global Factory-service in this file */
 
-  angular.module('myApp.factories', []).
-  factory('CurrencyFactory', function ($http) {
-      
-      var rates;
-      var status;
-      $http({
-        method: "GET",
-        url: "api/currency/rates"
-    })
-            .success(function(data, status, headers, config){
-                rates = data;
-                status = true;
-            })
-            .error(function(){
-                status = "Connection failed";
-            });
-      var info = "This is the CurrencyFactory";
-      var getInfo = function(){
-          return info;
-      };
-       var getRates = function(){
-          return rates;
-      };
-      var convertCurrency = function(amount, from, to){
-          var curr = getRates();
-                console.log(curr);
-      };
-    
-    return {
-        rates: rates,
-        status: status,
-        info: info,
-        getInfo: getInfo,
-        getRates: getRates,
-        convertCurrency: convertCurrency
-    };
-  })
-  
-  .factory('InfoFactory', function () {
-    var info = "Hello World from a Factory";
-    var getInfo = function getInfo(){
-      return info;
-    };
-    return {
-      getInfo: getInfo
-    };
-  });
+angular.module('myApp.factories', []).
+        factory('CurrencyFactory', function ($http) {
+            var factory = {};
+            factory.getData = function(){ // controller prints undefined when trying to print results..
+                return $http({
+                    method: "GET",
+                    url: "api/currency/rates"
+                });
+            };
+            return factory;
+        })
+
+        .factory('InfoFactory', function () {
+            var info = "Hello World from a Factory";
+            var getInfo = function getInfo() {
+                return info;
+            };
+            return {
+                getInfo: getInfo
+            };
+        });
--- web/app/view5/view5.js
+++ web/app/view5/view5.js
@@ -1,23 +1,31 @@
 'use strict';
 
 angular.module('myApp.view5', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view5', {
-    templateUrl: 'app/view5/view5.html',
-    controller: 'View5Ctrl'
-  });
-}])
-
-.controller('View5Ctrl', ['$http', '$scope', 'CurrencyFactory', function($http,$scope,CurrencyFactory) {
-    $scope.test = "this is a test";
-    $scope.rates = [];
-    $scope.status = "Connecting...";
-    $scope.status = CurrencyFactory.status;
-    $scope.rates = CurrencyFactory.getRates();
-    $scope.info = CurrencyFactory.getInfo();
-                console.log("printing rates here: " + $scope.rates);            
-    $scope.convert = function(amount, from, to){
+        
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view5', {
+                    templateUrl: 'app/view5/view5.html',
+                    controller: 'View5Ctrl'
+                });
+            }])
+        
+        .controller('View5Ctrl', ['$http', '$scope', 'CurrencyFactory', function ($http, $scope, CurrencyFactory) {
+//                $scope.conversions = {"Danish Kroner": 1};
+//                        $scope.status = CurrencyFactory.status;
+                $scope.amount = 0;
+                $scope.calculatedValue = 0;
+                $scope.fromCurrency = null;
+                $scope.toCurrency = null;
+                $scope.currencies = [];
+                CurrencyFactory.getData().success(function (data) {
+                    $scope.currencies = data;
+//                    for (var i = 0; i<data.length; i++) {
+//                        $scope.conversions.create(data.description, data.rate);
+//                    }
                     
-    };
-}]);
+//                    console.log($scope.conversions);
+                });
+                $scope.calc = function () {
+                    $scope.calculatedValue = $scope.amount * $scope.fromCurrency * $scope.toCurrency;
+                };
+            }]);
