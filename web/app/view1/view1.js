26772b66f0a2ba7ae541c28fcf66b36ae98c3f58
Rasmus <Rasmus@RL>
2016-04-15 12:54:00 +0200

    Finale

--- web/app/view1/view1.js
+++ web/app/view1/view1.js
@@ -2,15 +2,31 @@
 
 angular.module('myApp.view1', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'app/view1/view1.html',
-    controller: 'View1Ctrl',
-    controllerAs : 'ctrl'
-  });
-}])
+        .config(['$routeProvider', function ($routeProvider) {
+                $routeProvider.when('/view1', {
+                    templateUrl: 'app/view1/view1.html',
+                    controller: 'developerCtrl'
+                });
+            }])
 
-.controller('View1Ctrl', ["InfoFactory","InfoService",function(InfoFactory,InfoService) {
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+        .controller('developerCtrl', ['$http', '$scope', '$route', 'DocumentationFactory', function ($http, $scope, $route, DocumentationFactory) {
+                $scope.newEntry = {};
+                $scope.newEntry.number = 0;
+                $scope.newEntry.header = "";
+                $scope.newEntry.text = "";
+                $scope.documentation = [];
+                DocumentationFactory.getData().success(function (data) {
+                    $scope.documentation = data;
+                });
+                $scope.addDocumentEntry = function(){
+                    DocumentationFactory.addData($scope.newEntry);
+                    $scope.documentation.push($scope.newEntry)
+                    $route.reload();
+                    
+                };
+                $scope.removeEntry = function(entryId){
+                    DocumentationFactory.removeData(entryId);
+                    $route.reload();
+                };
+            }]);
+
